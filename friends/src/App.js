42725307fb37117b6cac9ff6edc3451fb4ae042c
CatsAreMLG <joshuadgon@gmail.com>
2019-02-05 20:35:49 -0600

    Create components that diplay friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+
+import FriendsList from "./components/FriendsList";
+import CreateFriendForm from "./components/CreateFriendForm";
+import UpdateFriendForm from "./components/UpdateFriendForm";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/friends").then(res => {
+      this.setState({ friends: res.data });
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <FriendsList friends={this.state.friends} />
       </div>
     );
   }
